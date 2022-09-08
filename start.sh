436833a16fa7f5f12ae39130d99d9c0f349ed156
Ashk <timed2a@hotmail.fr>
2022-09-08 23:07:12 +0200

    ✨ Simplify setup

--- start.sh
+++ start.sh
@@ -1,10 +1,36 @@
 #!/bin/bash
 
-base="mkcert -cert-file certs/local.pem -key-file certs/local-key.pem $(< domains.txt)"
+UNAMEOUT="$(uname -s)"./
 
-docker-compose down
+# Verify operating system is supported...
+case "${UNAMEOUT}" in
+Linux*) MKCERT_BIN=./bin/mkcert ;;
+Darwin*) MKCERT_BIN=mkcert ;;
+*) MKCERT_BIN="UNKNOWN" ;;
+esac
 
-echo $base
-$base
+# Unknown OS ? Where are you ?!
+if [ "$MKCERT_BIN" == "UNKNOWN" ]; then
+    echo "Unsupported operating system [$(uname -s)]. Supports macOS, Linux, and Windows (WSL2)." >&2
+    exit 1
+fi
 
-docker-compose up -d --build
+# Define Docker Compose command prefix...
+docker compose &>/dev/null
+if [ $? == 0 ]; then
+    DOCKER_COMPOSE="docker compose"
+else
+    DOCKER_COMPOSE="docker-compose"
+fi
+
+# Build command
+BASE="$MKCERT_BIN -cert-file certs/local.pem -key-file certs/local-key.pem $(< domains.txt)"
+
+# Down previous container
+$DOCKER_COMPOSE down 2>/dev/null
+
+# Register self trusted certificates
+$BASE
+
+# Rebuild container
+$DOCKER_COMPOSE up -d --build
